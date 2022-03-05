ed027e5d894c06359832a30eb9dec8c664462434
Zane Wilson <wilsonzanea@gmail.com>
2022-03-05 16:22:59 -0500

    find me section logos done

--- components/findMe.jsx
+++ components/findMe.jsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
 import styles from "./findMe.module.css";
-import { FaGithub } from "react-icons/fa";
-import { ImMail4 } from "react-icons/im";
+import {
+    FaGithub,
+    FaEnvelope,
+    FaLinkedinIn,
+    FaTwitter,
+    FaInstagram,
+    FaFacebookF,
+    FaYoutube,
+} from "react-icons/fa";
+
+import { SiGmail } from "react-icons/si";
 
 export default function ContactMe() {
     return (
@@ -10,8 +19,29 @@ export default function ContactMe() {
             <div className={`${styles.bubble} ${styles.githubBubble}`}>
                 <FaGithub className={`${styles.icon} ${styles.githubIcon}`} />
             </div>
-            <div className={`${styles.bubble} ${styles.emailBubble}`}>
-                <ImMail4 className={`${styles.icon} ${styles.emailIcon}`} />
+            <div className={`${styles.bubble} ${styles.gmailBubble}`}>
+                <SiGmail className={`${styles.icon} ${styles.gmailIcon}`} />
+            </div>
+            <div className={`${styles.bubble} ${styles.linkedInBubble}`}>
+                <FaLinkedinIn
+                    className={`${styles.icon} ${styles.linkedInIcon}`}
+                />
+            </div>
+            <div className={`${styles.bubble} ${styles.twitterBubble}`}>
+                <FaTwitter className={`${styles.icon} ${styles.twitterIcon}`} />
+            </div>
+            <div className={`${styles.bubble} ${styles.instagramBubble}`}>
+                <FaInstagram
+                    className={`${styles.icon} ${styles.instagramIcon}`}
+                />
+            </div>
+            <div className={`${styles.bubble} ${styles.facebookBubble}`}>
+                <FaFacebookF
+                    className={`${styles.icon} ${styles.facebookIcon}`}
+                />
+            </div>
+            <div className={`${styles.bubble} ${styles.youtubeBubble}`}>
+                <FaYoutube className={`${styles.icon} ${styles.youtubeIcon}`} />
             </div>
         </section>
     );
