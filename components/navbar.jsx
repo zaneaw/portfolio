be3b9c4d089fcb27a086678f932f5d66e705f72b
Zane Wilson <wilsonzanea@gmail.com>
2022-07-14 16:52:24 -0400

    WIP

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -53,26 +53,24 @@ export default function Navbar({ handleOpenFeedback }) {
         <header className={`mt-16 z-10`}>
             <nav className={`${!showNav && '-translate-y-14'} w-full bg-blue-dark p-2 flex justify-between items-center fixed top-0 transition-all duration-200 ease-out`}>
                 <Link href='#' passHref>
-                    <FaHeart size={40} color={'#006D77'} />
+                    <div>
+                        <FaHeart size={40} color={'#006D77'} />
+                    </div>
                 </Link>
 
                 <button
-                    className={`${
-                        isNavbarOpen && styles.active
-                    } bg-blue-dark rounded block p-1`}
+                    className={`${isNavbarOpen && styles.active} bg-blue-dark rounded block p-1`}
                     onClick={toggleNavbar}
                     aria-label='toggle navigation'
                 >
                     <span
-                        className={`${styles.bar} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out ${
-                            isNavbarOpen && console.log('Yes')
-                        }`}
+                        className={`${isNavbarOpen && 'translate-y-[10px] rotate-45'} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out`}
                     ></span>
                     <span
-                        className={`${styles.bar} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out`}
+                        className={`${isNavbarOpen && 'opacity-0'} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out`}
                     ></span>
                     <span
-                        className={`${styles.bar} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out`}
+                        className={`${isNavbarOpen && '-translate-y-[10px] -rotate-45'} block w-[35px] h-[5px] bg-blue rounded my-[5px] mx-auto transition-all duration-200 ease-in-out`}
                     ></span>
                 </button>
                 <div
@@ -120,7 +118,9 @@ export default function Navbar({ handleOpenFeedback }) {
             </nav>
             <div className='z-10 p-1.5 fixed right-4 bottom-4 rounded-full cursor-pointer bg-blue shadow opacity-75 hover:animate-pulse transition-all duration-300 ease-in-out hover:scale-95'>
                 <Link href='#' passHref>
-                    <FaArrowUp size={24} color={'#83C5BE'} />
+                    <div>
+                        <FaArrowUp size={24} color={'#83C5BE'} />
+                    </div>
                 </Link>
             </div>
         </header>
