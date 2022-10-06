1f05d74fa3197abd250904b538afd9fd56b8148c
Zane Wilson <wilsonzanea@gmail.com>
2022-10-05 22:59:29 -0400

    display position, start date and end date

--- components/blocks/ProjectDisplay.jsx
+++ components/blocks/ProjectDisplay.jsx
@@ -18,6 +18,13 @@ export default function projectDisplay({ project, isInView }) {
                 {project.title}
             </a>
 
+            {project.position && (
+                <span className="-mt-3 text-primary-light opacity-80 flex flex-wrap justify-between items-center text-base xs:text-lg lg:text-xl transition-all duration-300 ease-in">
+                    <p className="mr-auto italic hover:text-primary transition-all duration-300 ease-in">{project.position}</p>
+                    <p className="ml-auto italic hover:text-primary transition-all duration-300 ease-in">{project.startDate} - {project.endDate}</p>
+                </span>
+            )}
+
             <p>{project.desc}</p>
 
             <div className='flex flex-row flex-wrap font-monoCustom xs:text-lg lg:text-xl gap-x-4 gap-y-1'>
