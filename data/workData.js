adb4bb13bb67dfb8ba28feae1c04707b30aa0bc3
Zane Wilson <wilsonzanea@gmail.com>
2022-11-28 17:24:01 -0500

    work experience to work data file

---
+++ data/workData.js
@@ -0,0 +1,18 @@
+const workData = [
+    {
+        title: 'Share-a-Cart.com',
+        desc: 'Develop and update new and existing pages, components, and animations for a clean and pixel-perfect user interface. Develop and maintain a user experience focused on ease of use, reducing visual and functional bugs by ~15%. Develop and design new, complex user interaction flows, such as a system to compare current cart items to other vendors in order to save users money and to increase conversion rate. Stack includes JavaScript, Node, Express, EJS, and Tailwind.',
+        stack: ['Node.js', 'Tailwind CSS', 'jQuery', 'Express.js', 'EJS'],
+        sourceCode: '',
+        youtube: '',
+        liveSiteUrl: 'https://share-a-cart.com/',
+        liveSiteImg: 'sac.jpg',
+        width: 100,
+        height: 100,
+        position: 'Front End Developer',
+        startDate: '05/2022',
+        endDate: 'Present'
+    },
+];
+
+export default workData;
